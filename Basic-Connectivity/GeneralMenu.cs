3b54bd8083dc806d898065a3f92113b18bc1e4cf
Yusuf Valent Adyatomo <yusufvalent@gmail.com>
2023-09-20 16:09:56 +0700

    feature: adding generalmenu feature

---
+++ Basic-Connectivity/GeneralMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Connectivity
+{
+    public class GeneralMenu
+    {
+        public static void List<T>(List<T> items, string title)
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine($"List of {title}");
+            Console.WriteLine("---------------");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("---------------");
+        }
+    }
+}
